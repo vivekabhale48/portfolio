4f8237d5562675714b3c357c02fb1ed5311570bf
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2025-10-31 17:22:34 +0530

    Fixed Experience and Card sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import NavBar from "./components/NavBar"
+import Experience from "./sections/Experience"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import LogoShowcase from "./sections/LogoShowcase"
 import AppShowcase from "./sections/ShowcaseSection"
@@ -12,6 +14,8 @@ function App() {
       <Hero />
       <AppShowcase />
       <LogoShowcase />
+      <FeatureCards />
+      <Experience />
     </>
   )
 }
